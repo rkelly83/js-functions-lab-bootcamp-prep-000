d858e62f4f32e374c4116f562560bbbc1d05d0e1
Robert Kelly <robert.kelly83@gmail.com>
2018-04-08 20:34:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
+<<<<<<< HEAD
 function holidayCountdown(holiday, days){
+=======
+function holidayCountdown(days, holiday){
+>>>>>>> c75dec5345692621115393bb4fe3da11c9c38eb4
   return `It's ${days} days until ${holiday}!`
 }
